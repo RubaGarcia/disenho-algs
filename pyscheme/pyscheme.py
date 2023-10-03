2f16cfff9b24e6bf44cd6450086c317a7066436a
Rubén García <rgm177@alumnos.unican.es>
2023-10-03 18:28:58 +0200

    primera iteracion
    Co-authored-by: MIER MARTIN, JAVIER <javier.mierma@alumnos.unican.es>
    Co-authored-by: FERNANDEZ CABRERO, CARLOS <carlos.fernandezcab@alumnos.unican.es>
    Co-authored-by: yuhuaZhn <yuhuaZhn@users.noreply.github.com>

--- pyscheme/pyscheme.py
+++ pyscheme/pyscheme.py
@@ -18,18 +18,31 @@ fact = ('define', 'fact',
                             ('*', 'n', ('fact', ('-', 'n', 1))))))
 
 
-env = {'+': lambda x,y: x+y 
 
+
+env = {'+': lambda x,y: x+y,
+        '-': lambda x,y: x-y,
+        '*': lambda x,y: x*y,
+        '=': lambda x,y: x==y,
+        'define': lambda x,y: env.update({x:y}),
+        'lambda': lambda x,y: seval([seval(y),x]),
+        'if': lambda x,y,z: seval(y) if seval(x) else seval(z)
     }
+
+
 
 # You will define the following procedure for evaluating an expression
 def seval(sexp):
     if isinstance(sexp,int):
         return sexp
+    if isinstance(sexp,str):
+        return env.get(sexp,sexp)
     elif isinstance(sexp, tuple):
         func = env[sexp[0]]
         args = [seval(e) for e in sexp[1:]]
         return func(*args)
+    
+    
 
 # In writing seval, you are ONLY allowed to use the rules of Scheme
 # evaluation that you currently know about.  So far, this includes the
