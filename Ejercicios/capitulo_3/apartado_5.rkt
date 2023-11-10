1788f81e37b80cad78e1d9b2f1122f1a7dc53bdc
yuhuaZhn <126669572+yuhuaZhn@users.noreply.github.com>
2023-11-10 17:28:55 +0100

    Update apartado_5.rkt

--- Ejercicios/capitulo_3/apartado_5.rkt
+++ Ejercicios/capitulo_3/apartado_5.rkt
@@ -5,3 +5,6 @@
   (cond ((and (= n k) (= n 0)) 1)
         ((and (>= n 0) (>= k 1)) (+ (p_act n (- k 1)) (p_act (- n k) k)))
         (else 0)))
+
+;; la complejidad espacial seria n * n
+;; la complejidad temporal es 2 ^ n ya que para cada elemento se calculan otros dos
